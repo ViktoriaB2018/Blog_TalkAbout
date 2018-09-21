4731db00a29ada97a3057f6e85cec0c4ec2da16e
ViktoriaB2018 <bvo2008@mail.ru>
2018-09-21 16:18:56 +0300

    Add comments

--- app.rb
+++ app.rb
@@ -10,11 +10,16 @@ def init_db
 end
 
 before do
+	# инициализация БД
 	init_db
 end
 
+# вызывается каждый раз при конфигурации приложения: когда изменился код программы и перезагрузилась страница
 configure do
+	# инициализация БД
 	init_db
+
+	# создать таблицу, если таблицы не существует
 	@db.execute 'CREATE TABLE IF NOT EXISTS Posts (id INTEGER PRIMARY KEY AUTOINCREMENT, created_data DATA, content TEXT NOT NULL)'
 end
 
@@ -22,11 +27,16 @@ get '/' do
 	erb "Hello!"	
 end
 
+# обработчик get-запроса /new
+# браузер получет страницу с сервера
 get '/new' do
 	erb :new
 end
 
+# обработчик post-запроса /new
+# браузер отправляет данные на сервер
 post '/new' do
+	# получаем переменную из пост запроса
 	@content = params[:content]
 	erb "You tiped #{@content}"
 end
